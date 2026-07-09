fa34189e8c449e4b690b61f29240dae0013a2d94
Byron Alvarez <bvalvarez1.1@gmail.com>
2026-07-09 12:39:28 -0500

    update: Database Postgres settings

--- taller/condominio/condominio/settings.py
+++ taller/condominio/condominio/settings.py
@@ -98,8 +98,12 @@ WSGI_APPLICATION = 'condominio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'condominio',       
+        'USER': 'user',       
+        'PASSWORD': 'password', 
+        'HOST': 'localhost',  
+        'PORT': '5434',       
     }
 }
 
