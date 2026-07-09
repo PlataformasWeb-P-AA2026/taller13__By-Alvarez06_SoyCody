d52648a16c692483ad570221f4fa603f02a34382
Cody <codycabreraa@gmail.com>
2026-07-09 12:41:17 -0500

    index2

--- taller/flask-app/app.py
+++ taller/flask-app/app.py
@@ -3,6 +3,7 @@ import requests
 import json 
 
 app = Flask(__name__, template_folder='templates')
+app.config['SECRET_KEY'] = 'una-clave-secreta-000001'
 
 token = '493346043f223ae9890d44a30ec2771966f40744'
 headers = {
@@ -12,8 +13,21 @@ headers = {
 
 @app.route("/")
 def home():
-    
-    return "¡Hola mundo!"
+    """
+    """
+    r_edificios = requests.get("http://localhost:8000/api/edificios/",
+            auth=('cody', 'admin123.'))
+    edificios = json.loads(r_edificios.content)['results']
+    numero_edificios = json.loads(r_edificios.content)['count']
+
+    r_departamentos = requests.get("http://localhost:8000/api/departamentos/",
+            headers=headers)
+    departamentos = json.loads(r_departamentos.content)['results']
+    numero_departamentos = json.loads(r_departamentos.content)['count']
+
+    return render_template("index.html", edificios=edificios,
+    numero_edificios=numero_edificios, departamentos=departamentos,
+    numero_departamentos=numero_departamentos)
 
 
 
